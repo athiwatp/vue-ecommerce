b11dc4cbc993db98ed49eefb0566a8acc7501970
Sendy Kou <koucxz@hotmail.com>
2017-03-24 14:09:43 +0800

    vue-cli,webpack完成的电商项目

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -10,14 +10,18 @@ let url = {
 url = rap(url)
 
 new Vue({
-  el: '.recommendation',
+  el: '.main-content-w',
   data: {
     excavatorList: '',
-    partsList: ''
+    partsList: '',
+    slideList: '',
+    currentIndex: 0,
+    timer: ''
   },
   created() {
     this.getList()
     this.getList(3)
+    this.getSlideList()
   },
   methods: {
     getList(type = undefined) {
@@ -30,6 +34,43 @@ new Vue({
           this.excavatorList = res.data.merchandiseHotVOList
         }
       })
+    },
+    getSlideList() {
+      fetch(url.slideList).then(res => {
+        this.slideList = res.data.slideList
+        this.$nextTick(() => {
+          this.timer = setInterval(() => {
+            this.autoPlay()
+          },4000)
+        })
+      })
+    },
+    go() {
+      this.timer = setInterval(() => {
+        this.autoPlay()
+      },4000)
+    },
+    stop() {
+      clearInterval(this.timer)
+      this.timer = null
+    },
+    reduceNum(list) {
+      if (list.num <= 1) {
+        return
+      }
+      list.num--
+    },
+    addNum(list) {
+      list.num++
+    },
+    change(index) {
+      this.currentIndex = index
+    },
+    autoPlay() {
+      this.currentIndex++
+      if (this.currentIndex > this.slideList.length - 1) {
+        this.currentIndex = 0
+      }
     }
   }
 })
