1a28d0e3652752d9fd6be15803372a794ddc1107
Sendy Kou <koucxz@hotmail.com>
2017-04-02 10:20:06 +0800

    vue-cli,webpack完成的电商项目

--- src/pages/forgetPwd/forgetPwd.js
+++ src/pages/forgetPwd/forgetPwd.js
@@ -1,15 +1,6 @@
 import 'normalize.css'
 import 'pages/register/register.scss'
 
-
-import { fetch, rap } from 'js/fetch.js'
-
-let url = {
-  resetPwd: '/user/resetPwd',
-  getCode: '/user/getCodeForForget'
-}
-url = rap(url);
-
 import Top from 'components/top/top.vue'
 import Foot from 'components/foot/foot.vue'
 import Register from 'components/register/register.vue'
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,14 +1,6 @@
 import 'normalize.css'
 import './register.scss'
 
-import { fetch, rap } from 'js/fetch.js'
-
-let url = {
-  register: '/user/register.do',
-  getCode: '/user/getCode.do'
-}
-url = rap(url)
-
 import Top from 'components/top/top.vue'
 import Foot from 'components/foot/foot.vue'
 import Register from 'components/register/register.vue'
