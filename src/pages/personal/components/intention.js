4a43dcbc2fcc807c9f34c54b504803a2b612f1ae
Sendy Kou <koucxz@hotmail.com>
2017-05-01 23:47:18 +0800

    订单提交删除操作

--- src/pages/personal/components/intention.js
+++ src/pages/personal/components/intention.js
@@ -29,7 +29,7 @@ export default {
         })
       },
       cancel(id,index) {
-        MessageBox.confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        MessageBox.confirm('此操作将删除该订单, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
--- src/pages/personal/components/order.js
+++ src/pages/personal/components/order.js
@@ -1,5 +1,7 @@
 import Pagination from 'components/pagination/pagination.vue'
 import order from 'js/orderService.js'
+import { MessageBox } from 'element-ui'
+import { Message } from 'element-ui'
 
 export default {
   data() {
@@ -27,13 +29,70 @@ export default {
       })
     },
     cancel(orderId) {
-
+      MessageBox.confirm('此操作将取消该订单, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        order.cancel({
+          orderId: orderId
+        }).then(res => {
+          Message({
+            type: 'success',
+            message: '取消订单成功!'
+          })
+          this.getLists()
+        })
+      }).catch(() => {
+        Message({
+          type: 'info',
+          message: '未取消订单'
+        });
+      });
     },
     remove(orderId) {
-
+      MessageBox.confirm('此操作将删除该订单, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        order.delete({
+          orderId: orderId
+        }).then(res => {
+          Message({
+            type: 'success',
+            message: '删除订单成功!'
+          })
+          this.getLists()
+        })
+      }).catch(() => {
+        Message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     },
     sign(orderId) {
-
+      MessageBox.confirm('是否确定已收到货物?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        order.sign({
+          orderId: orderId
+        }).then(res => {
+          Message({
+            type: 'success',
+            message: '已确认收货!'
+          })
+          this.getLists()
+        })
+      }).catch(() => {
+        Message({
+          type: 'info',
+          message: '已取消'
+        });
+      });
     },
     change(page) {
       this.pageNum = page
